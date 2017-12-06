76cdc68eecde92630f0539c3ad744449cd9133b6
Shayti <shytinte@gmail.com>
2017-12-05 22:06:46 -1100

    existing file

---
+++ WEBTOON/www/js/process.js
@@ -0,0 +1,148 @@
+// import 'imports-loader?exports=>false,define=>false,this=>window!framework7';
+// var myApp = new Framework7();
+// var $$ = Dom7;
+// $$('img.lazy').trigger('lazy');
+ 
+// $$('div.lazy').trigger('lazy');
+
+// myApp.initImagesLazyLoad(div);
+let process = {
+
+   Start:function(){
+let p = <div class="katawan">
+            <div class="line1">WEBTOON<br/><br/></div>
+            <div><a href="#" onClick={process.page1}><center><button>LOOKISM</button></center></a></div>
+        </div>;
+        ReactDOM.render(p, document.getElementById("r"));
+    },
+    page1:function(){
+        let p = <div>
+                    <div class="line1"><center>LOOKISM</center></div><br/>
+                    <div>
+   
+    <img src="img/1.png" />
+    <img src="img/2.png" />
+    <img src="img/3.png" />
+    <img src="img/4.png" />
+    <img src="img/5.png" />
+    <img src="img/6.png" />
+    <img src="img/7.png" />
+    <img src="img/8.png" />
+    <img src="img/9.png" />
+    <img src="img/10.png" />
+    <img src="img/11.png" />
+    <img src="img/12.png" />
+    <img src="img/13.png" />
+    <img src="img/14.png" />
+    <img src="img/15.png" />
+    <img src="img/16.png" />
+    <img src="img/17.png" />
+    <img src="img/18.png" />
+    <img src="img/19.png" />
+    <img src="img/20.png" />
+    <img src="img/21.png" />
+    <img src="img/22.png" />
+    <img src="img/23.png" />
+    <img src="img/24.png" />
+    <img src="img/25.png" />
+    <img src="img/26.png" />
+    <img src="img/27.png" />
+    <img src="img/28.png" />
+    <img src="img/29.png" />
+    <img src="img/30.png" />
+    <img src="img/31.png" />
+    <img src="img/32.png" />
+    <img src="img/33.png" />
+    <img src="img/34.png" />
+    <img src="img/35.png" />
+    <img src="img/36.png" />
+    <img src="img/37.png" />
+    <img src="img/38.png" />
+    <img src="img/39.png" />
+    <img src="img/40.png" />
+    <img src="img/41.png" />
+    <img src="img/42.png" />
+    <img src="img/43.png" />
+    <img src="img/44.png" />
+    <img src="img/45.png" />
+    <img src="img/46.png" />
+    <img src="img/47.png" />
+    <img src="img/48.png" />
+    <img src="img/49.png" />
+    <img src="img/50.png" />
+    <img src="img/51.png" />
+    <img src="img/52.png" />
+    <img src="img/53.png" />
+    <img src="img/54.png" />
+    <img src="img/55.png" />
+    <img src="img/56.png" />
+    <img src="img/57.png" />
+    <img src="img/58.png" />
+    <img src="img/59.png" />
+    <img src="img/60.png" />
+    <img src="img/61.png" />
+    <img src="img/62.png" />
+    <img src="img/63.png" />
+    <img src="img/64.png" />
+    <img src="img/65.png" />
+    <img src="img/66.png" />
+    <img src="img/67.png" />
+    <img src="img/68.png" />
+    <img src="img/69.png" />
+    <img src="img/70.png" />
+    <img src="img/71.png" />
+    <img src="img/72.png" />
+    <img src="img/73.png" />
+    <img src="img/74.png" />
+    <img src="img/75.png" />
+    <img src="img/76.png" />
+    <img src="img/77.png" />
+    <img src="img/78.png" />
+    <img src="img/79.png" />
+    <img src="img/80.png" />
+    <img src="img/81.png" />
+    <img src="img/82.png" />
+    <img src="img/83.png" />
+    <img src="img/84.png" />
+    <img src="img/85.png" />
+    <img src="img/86.png" />
+    <img src="img/87.png" />
+    <img src="img/88.png" />
+                        <center>
+                            <a href="#" onClick={process.char}>
+                                <div class="ks-demo-icon col-auto tablet-auto">
+                                    <div class="icon" style={{color:"blue"}}><i class="material-icons">Go to top</i></div>
+                                </div>
+                            </a>
+                        </center>
+                    </div>
+            </div>;
+            ReactDOM.render(p,document.getElementById('r'));
+    },
+
+    G:function(){
+    let p=<center>
+        <div class="katawan"><br/><br/><br/>
+            <a class="line">WELCOME TO MY WEBTOON</a>
+        </div></center>;
+         ReactDOM.render(p,document.getElementById('r'));
+    }
+}
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+
+
+
+
+
+
+process.G();
+setTimeout(function(){
+    process.Start();
+},2000);
